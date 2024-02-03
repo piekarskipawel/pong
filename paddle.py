08aee641f46a35e1170b4c1dac3b7a0dc773636c
Pawel Piekarski <pawpiek@gmail.com>
2024-02-03 17:31:37 +0100

    one paddle

---
+++ paddle.py
@@ -0,0 +1,19 @@
+import pygame
+from settings import Settings
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.settings = Settings()
+        self.image = pygame.Surface((20, 100))
+        self.image.fill(self.settings.paddle_color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (50, self.settings.screen_height // 2)
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.top > 0:
+            self.rect.y -= self.settings.speed_paddle_one
+        if keys[pygame.K_DOWN] and self.rect.bottom < self.settings.screen_height:
+            self.rect.y += self.settings.speed_paddle_one
+
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+        self.paddle_color = (255,255,255)
+        self.speed_paddle_one = 5
